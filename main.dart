3fdb9451cde785a7a9edab76f8d9f9600dd09eb0
SwaqsX <119972020+SwaqsX@users.noreply.github.com>
2023-01-05 20:09:04 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import 'package:nalan/screens/todolist.dart';
+
+// ignore: unused_import
+
+void main(List<String> args) {
+  runApp(const App());
+}
+
+/*class App extends StatelessWidget {
+  const App({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Benim Projem',
+      theme: ThemeData(primarySwatch: Colors.teal),
+      home: const Toplama(),
+    );
+  }
+}*/
+/*class App extends StatelessWidget {
+  const App({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Benim Projem',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const Nalan(),
+    );
+  }
+}*/
+class App extends StatelessWidget {
+  const App({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Benim Projem',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const Todolist(),
+    );
+  }
+}
